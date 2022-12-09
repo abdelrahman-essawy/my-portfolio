b265e8f7254b58eddf6dd58e6d78592fd0a841bd
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-09 23:39:48 +0200

    removing href

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -35,7 +35,7 @@ export default function Hero() {
           <h1 className="my-4 sm:mt-0 text-4xl font-medium tracking-normal text-gray-800 md:text-5xl lg:text-5xl leading-2"><span className='sm:leading-loose md:leading-tight'><span className='text-gray-800 font-base'>Abdelrahman</span> <span className='text-gray-800 font-bold  decoration-3 underline-offset-8 decoration-blue-500'>Essawy</span></span> </h1>
           <p id='glitch' className="mt-8 mb-2 text-xl font-normal text-gray-500 lg:text-2xl">I&apos;m a <span className='type-jobs  font-bold'></span></p>
           <div className="hidden  md:justify-start mb-8 sm:my-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <p href="#" className="hidden sm:inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
+            <p className="hidden sm:inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
               Learn more
               <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
             </p>
