1f067c7166ce56f9401f1d9d1132f6321262f068
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-10 16:16:57 +0200

    ipmroving navbar ui

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       </Head>
 
-      <header className='sticky top-0 z-50 backdrop-blur-3xl shadow-sm'>
+      <header className='sticky top-0 z-50 shadow-sm bg-white/50 backdrop-blur-3xl'>
       <Spliiter />
       <Navbar />
       {/* <div className='h-[1.5px] opacity-40 bg-gradient-to-r from-emerald-500 via-blue-500 to-emerald-500'></div> */}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Example() {
     <Disclosure as="nav">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative  flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
