e63f8a74d62b8c6c1ac769e37c788dc3ab696a8b
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-10 22:14:18 +0200

    adding framer motion

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar';
 import Testimonials from '../components/Testimonials';
 import Hero from './../components/Hero';
 import Spliiter from './../components/Spliiter';
+import {motion} from 'framer-motion'
 
 export default function Home() {
   return (
@@ -16,18 +17,29 @@ export default function Home() {
 
       </Head>
       <header className='sticky top-0 z-50 shadow-sm bg-white/50 backdrop-blur-3xl'>
-      <Spliiter/>
-      <Navbar />
+        <Spliiter />
+        <Navbar />
       </header>
 
       {/* <div className='h-[1.5px] opacity-40 bg-gradient-to-r from-emerald-500 via-blue-500 to-emerald-500'></div> */}
-      <Hero />
-      <Spliiter />
-      <About />
-      <Spliiter />
-      <Testimonials />
-      <Spliiter />
-      
+      <motion.main
+        initial={{
+          opacity: 0,
+          y: 5
+        }}
+        animate={{
+          opacity: 1,
+          y: 0
+        }}>
+
+        <Hero />
+        <Spliiter />
+        <About />
+        <Spliiter />
+        <Testimonials />
+        <Spliiter />
+
+      </motion.main>
 
     </>
   )
