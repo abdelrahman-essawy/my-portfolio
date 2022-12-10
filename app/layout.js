fb43e2fb47a25fc9e8117ebe6e0cdb65db701c46
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-10 17:29:56 +0200

    adding framer motion

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,12 @@
+"use client"
 import '../styles/globals.css'
+import {motion} from 'framer-motion'
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <motion.body initial={{opacity:0}} animate={{opacity:1}}>{children}</motion.body>
     </html>
   )
 }
