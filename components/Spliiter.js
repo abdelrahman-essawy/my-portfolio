96ecaeab3919300024414e28612ef6d214b8fd79
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-11 08:48:22 +0200

    adding sliding splitter motion

--- components/Spliiter.js
+++ components/Spliiter.js
@@ -5,8 +5,8 @@ import { inflate } from 'zlib'
 export default function Spliiter() {
   return (
     <m.div
-      initial={{ right: 0, opacity: 0 }}
-      whileInView={{ right: 'auto', opacity: 1 }}
+      initial={{ width: 0, opacity: 0 }}
+      whileInView={{ width: 'auto', opacity: 1 }}
       transition={{ duration: '.5' }}
       className='rounded-full z-50 h-1 bg-gradient-to-r from-emerald-500 via-blue-500 to-emerald-500'></m.div>
   )
