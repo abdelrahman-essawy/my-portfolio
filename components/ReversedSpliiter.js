457ef6189a8c2e83235eb330dae77e5a57e60138
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-11 08:19:26 +0200

    improving FIND ME IN ,  SPLITTER motion

--- components/ReversedSpliiter.js
+++ components/ReversedSpliiter.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { motion as m } from 'framer-motion'
 import { inflate } from 'zlib'
 
-export default function Spliiter() {
+export default function ReversedSpliiter() {
   return (
     <m.div
-      initial={{ right: 0, opacity: 0 }}
-      whileInView={{ right: 'auto', opacity: 1 }}
-      transition={{ duration: '.5' }}
+      initial={{ width: "0vw", x: "100vw" }}
+      whileInView={{ width: "50vw", x: 0 }}
+      transition={{ duration: 1, origin: 1 }}
       className='rounded-full z-50 h-1 bg-gradient-to-r from-emerald-500 via-blue-500 to-emerald-500'></m.div>
   )
 }
