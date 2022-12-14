5382e9a8513d87d49f8e58d01eaa2e77cd628570
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-14 23:47:14 +0200

    addiong reviews

--- components/Testimonials/Testimonials.jsx
+++ components/Testimonials/Testimonials.jsx
@@ -4,15 +4,15 @@ import Reviews from './Reviews';
 export default function Testimonials() {
 
     return (
-        <section id='TestimonialsID' className='bg-white py-6 px-4 sm:p-0 relative overflow-y sm:px-0'>
+        <section id='TestimonialsID' className='relative px-4 py-6 bg-white sm:p-0 overflow-y sm:px-0'>
             <div className='px-12 my-8 sm:mx-0'>
-                <h2 className='text-3xl tracking-tight text-blue-500 leading-2 text-center'>Testimonials</h2>
-                <h3 className='mt-3 font-medium  text-xl sm:tracking-wide text-gray-500 leading-2 text-center'>What My Clients Are Saying...</h3>
+                <h2 className='text-3xl tracking-tight text-center text-blue-500 leading-2'>Testimonials</h2>
+                <h3 className='mt-3 text-xl font-medium text-center text-gray-500 sm:tracking-wide leading-2'>What My Clients Are Saying...</h3>
             </div>
 
 
 
-            <div className='sm:px-0 my-8 sm:p-0 '>
+            <div className='my-8 sm:px-0 sm:p-0 '>
                 <Reviews />
             </div>
 
