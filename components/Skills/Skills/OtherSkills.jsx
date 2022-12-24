c23f09d6357fba9bdc942c93565313a95f0c6f86
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-25 00:08:40 +0200

    adding other skills icons

--- components/Skills/Skills/OtherSkills.jsx
+++ components/Skills/Skills/OtherSkills.jsx
@@ -4,59 +4,82 @@ import { useMeasure } from 'react-use-measure';
 
 export default function OtherSkills() {
 
+    const descTemp = (desc) => {
+        return (
+            <m.div
+                initial={{ opacity: 0, y: 5 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 5 }}
+
+                className='flex flex-col items-start justify-start'>
+                <div className='flex items-center justify-start'>
+                    <div className={`w-2 h-2 rounded-full ${desc}`}></div>
+                    <p className='ml-2 text-sm'>{desc}</p>
+                </div>
+
+            </m.div>
+        )
+    }
+
 
     const [otherSkills, setOtherSkills] = useState([
         {
             name: 'Networking',
             desc: 'I have a good understanding of networking and how it works. I have a good understanding of how to set up a network and how to troubleshoot it.',
-            color: 'bg-green-500',
             viewDesc: false,
+            icon: 'https://cdn-icons-png.flaticon.com/512/1239/1239682.png ',
         },
         {
             name: 'Linux',
             desc: 'I have a good understanding of Linux and how it works. I have a good understanding of how to set up a Linux server and how to troubleshoot it.',
-            color: 'bg-green-500',
             viewDesc: false,
+            icon: '   https://cdn-icons-png.flaticon.com/512/2333/2333187.png ',
         },
         {
             name: 'Cyber Security',
             desc: 'I have a good understanding of Cyber Security and how it works. I have a good understanding of how to set up a Cyber Security server and how to troubleshoot it.',
-            color: 'bg-green-500',
+            icon: '     https://cdn-icons-png.flaticon.com/512/2716/2716652.png  ',
         },
         {
             name: '3D Designer',
-            color: 'bg-green-500',
+            icon: 'https://cdn-icons-png.flaticon.com/512/1806/1806507.png ',
         },
         {
             name: 'Video Editing',
-            color: 'bg-green-500',
+            icon: '   https://cdn-icons-png.flaticon.com/512/2886/2886875.png ',
+            desc: [
+                {
+                    name: 'Adobe Premiere',
+                    color: 'bg-green-500',
+                },
+                {
+                    name: 'Adobe After Effects',
+                    color: 'bg-green-500',
+                },
+
+            ]
         },
         {
             name: 'Photography',
-            color: 'bg-green-500',
+            icon: '   https://cdn-icons-png.flaticon.com/512/1042/1042339.png ',
         },
-        {
-            name: 'Graphic Design',
-            color: 'bg-green-500',
-        },
-        {
-            name: 'Adobe Photoshop',
-            color: 'bg-green-500',
-        },
-        {
-            name: 'Adobe Illustrator',
-            color: 'bg-green-500',
-        },
-        {
-            name: 'Adobe XD',
-            color: 'bg-green-500',
 
-        },
-        {
-            name: 'Adobe Premiere',
-            color: 'bg-green-500',
-        },
     ])
+
+
+    // const oldArray = '
+
+    // desc[0] ?
+    //     <div className='flex flex-col items-start justify-start'>
+    //         {desc.map((desc, index) => (
+    //             <div key={index} className='flex items-center justify-start'>
+    //                 <div className={`flex-shrink - 0 w - 3 h - 3 ${desc.color} rounded - full`}></div>
+    //                 <div className='ml-2 text-sm font-medium text-gray-500'>{desc.name}</div>
+    //             </div>
+    //         ))}
+    //     </div>
+
+    //     : '
 
     return (
         <div className='grid grid-cols-1 gap-4 '>
@@ -64,7 +87,8 @@ export default function OtherSkills() {
                 name,
                 desc,
                 color,
-                viewDesc
+                viewDesc,
+                icon
 
             }, index) => (
                 <m.button
@@ -82,17 +106,21 @@ export default function OtherSkills() {
                     <div className='flex items-center justify-between p-3 bg-white rounded-lg shadow-md'>
                         <div className='flex items-center'>
                             <div className={`flex-shrink-0 w-10 h-10 ${color} rounded-full`}>
-                                <svg className='w-6 h-6 text-white' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-                                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M12 6v6m0 0v6m0-6h6m-6 0H6' />
-                                </svg>
+                                <img src={icon} alt={name} className='w-10 h-10' />
                             </div>
                             <div className='ml-4'>
                                 <div className='font-medium text-gray-700 text-md text-start sm:text-lg'>{name}</div>
-                                {viewDesc && <m.div
-                                    initial={{ height: 0, opacity: 0, }}
-                                    animate={{ height: 'auto', opacity: 1, type: 'spring', stiffness: 100, damping: 20, }}
-                                    key={index}
-                                    className='text-sm font-medium text-gray-500'>{desc}</m.div>}
+                                {
+
+
+                                    viewDesc
+                                    &&
+
+                                    descTemp(desc)
+
+
+                                }
+
 
                             </div>
                         </div>
@@ -103,9 +131,11 @@ export default function OtherSkills() {
                         </svg> : ''}
                     </div>
                 </m.button>
-            ))}
+            ))
+            }
         </div>
 
 
     )
 }
+
