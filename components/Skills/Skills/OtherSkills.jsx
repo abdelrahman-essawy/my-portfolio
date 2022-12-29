b5025e7cea17cb3fb3d9a314f60b247b7398897e
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-29 17:01:48 +0200

    fixing grid-cols-2

--- components/Skills/Skills/OtherSkills.jsx
+++ components/Skills/Skills/OtherSkills.jsx
@@ -154,7 +154,7 @@ export default function OtherSkills() {
 
 
     return (
-        <div className='grid grid-cols-2 gap-4 '>
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 '>
             {otherSkills.map(({
                 name,
                 desc,
