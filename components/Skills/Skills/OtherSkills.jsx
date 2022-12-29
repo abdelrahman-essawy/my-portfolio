388bc482a0eda00a0535922457dd0bb9597a1fe7
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-29 13:59:32 +0200

    chaning whileinview to animate

--- components/Skills/Skills/OtherSkills.jsx
+++ components/Skills/Skills/OtherSkills.jsx
@@ -156,7 +156,7 @@ export default function OtherSkills() {
             }, index) => (
                 <m.button
                     initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
+                    animate={{ opacity: 1, y: 0 }}
                     exit={{ opacity: 0, y: 20 }}
                     transition={{ delay: index * 0.1 }}
                     key={index}
